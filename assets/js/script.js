68f88c72d0f5f69455e1238092d1bc05c3994bfd
lacey aguigam <laceyaguigam@yahoo.com>
2022-08-23 10:05:42 -0700

    saving

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var searchBtn = document.getElementById("btnSearch")
+//search for a city,current and future conditions for that city and that city is added to the search history
+
+
+//view current weather conditions for that city, presented with the city name, the date, an icon representation of weather conditions, 
+//the temperature, the humidity, the wind speed, and the UV index
+
+
+//view the UV index, presented with a color that indicates whether the conditions are favorable, moderate, or severe
+
+
+//view future weather conditions for that city,THEN I am presented with a 5-day forecast that displays the date, an icon representation of 
+//weather conditions, the temperature, the wind speed, and the humidity
